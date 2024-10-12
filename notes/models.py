829bc86e6a64eaad0c376a2e91c4392eea7c0f36
Lucas Pagès <lucas@fedora.fritz.box>
2024-10-12 18:41:27 +0200

    Defined Note model + added test class for it

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Note(models.Model):
+    title = models.TextField(max_length=20, blank=True)
+    note = models.TextField(max_length=256, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_edited = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        if len(self.note) > 20:
+            max_length = 20
+        else:
+            max_length = len(self.note)
+        return f"{self.title} : {self.note[:max_length]}"
---
+++ notes/tests/test_models.py
@@ -0,0 +1,38 @@
+from django.test import TestCase
+from notes.models import Note
+from freezegun import freeze_time
+from datetime import datetime, timezone
+
+
+class NoteTest(TestCase):
+    def setUp(self):
+        pass
+
+    def test_empty_note(self):
+        note_obj = Note.objects.create()
+
+        self.assertEqual(note_obj.title, "")
+        self.assertEqual(note_obj.note, "")
+        self.assertNotEqual(note_obj.created_at, None)
+        self.assertNotEqual(note_obj.last_edited, None)
+    
+    @freeze_time("2024-10-12 18:17:00")
+    def test_creation_date_added(self):
+        note_obj = Note.objects.create()
+
+        self.assertEqual(note_obj.created_at, datetime.now(tz=timezone.utc))
+
+    def test_edited_correct_date(self):
+        note_obj = Note.objects.create()
+
+        note_obj.title = "test title"
+        freezer = freeze_time("2024-10-12 18:17:00")
+        freezer.start()
+        note_obj.save()
+
+        self.assertEqual(note_obj.last_edited, datetime.now(tz=timezone.utc))
+        self.assertNotEqual(note_obj.created_at, datetime.now(tz=timezone.utc))
+        freezer.stop()
+
+        
+
