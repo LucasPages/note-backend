a1d9ee222abe053217ad0f71eba61654f08edd3f
Lucas Pagès <lucas@fedora.fritz.box>
2024-10-12 16:52:32 +0200

    set up local postgresql database in settings.py + added .gitignore from GitHub

--- backend_project/settings.py
+++ backend_project/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
+from dotenv import load_dotenv
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,10 +78,15 @@ WSGI_APPLICATION = 'backend_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+load_dotenv("/home/lucas/Documents/web/note_backend/backend_project/.env")
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'note_app',
+        'USER': 'lucas',
+        'HOST': 'localhost',
+        'PASSWORD': os.getenv("DB_PASSWORD")
     }
 }
 
