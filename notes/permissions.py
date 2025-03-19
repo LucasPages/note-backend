f04832839d804a2005b09529a16186829d56c36a
Lucas Pagès <lucas.pages123@gmail.com>
2025-03-19 23:46:50 +0100

    removing IsUser permission

--- notes/permissions.py
+++ notes/permissions.py
@@ -1,18 +1,6 @@
 from rest_framework.permissions import BasePermission
 
 
-class IsUser(BasePermission):
-    def has_permission(self, request, view):
-        if view.action == 'list':
-            return request.user.is_superuser
-        return True
-    
-    def has_object_permission(self, request, view, obj):
-        if view.action == 'retrieve':
-            return obj.id == request.user.id or request.user.is_superuser
-        return obj.id == request.user.id
-
-
 class IsAuthorOfNote(BasePermission):
     def has_permission(self, request, view):
         if view.action == 'list':
