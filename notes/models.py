101f35329a64b5cc52c64c4af83c33be61a3a821
Lucas Pagès <lucas.pages123@gmail.com>
2025-03-21 23:54:51 +0100

    adding unique condition on tag names

--- notes/models.py
+++ notes/models.py
@@ -22,7 +22,7 @@ class Note(models.Model):
 
 class Tag(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=20)
+    name = models.CharField(unique=True, max_length=20)
     notes = models.ManyToManyField(Note, related_name='tags')
 
     def __str__(self) -> str:
